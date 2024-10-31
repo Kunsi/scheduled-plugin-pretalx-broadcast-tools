cc8bcc1233ea4bc15645e9e51050a8bbd892b1dc
Franziska Kunsmann <hi@kunsmann.eu>
2024-10-31 21:54:24 +0100

    add option to use room uuid instead of room name

--- tile.lua
+++ tile.lua
@@ -139,7 +139,7 @@ local function check_next_talks()
         -- to announce these.
         if talk.end_ts > time and talk.start_ts > min_start then
             -- is this in *this* room, or somewhere else?
-            if current_room and talk.room == current_room then
+            if current_room and (talk.room == current_room or talk.room_uuid == current_room) then
                 room_next_talks[#room_next_talks+1] = talk
             end
             all_next_talks[#all_next_talks+1] = talk
